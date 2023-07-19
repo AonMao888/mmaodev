76d3680e171429b900901b3e6697dce1385fb3cf
Aon Mao <aonmao888@gmail.com>
2023-07-19 22:38:16 +0630

    7

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const ejs = require('ejs');
 const app = express();
+require('dotenv').config();
+const {createClient} = require('@supabase/supabase-js');
+
+const supabase = createClient(process.env.supabaseurl,process.env.supabasekey)
 
 app.set('views',__dirname+'/views')
 app.set('view engine','ejs')
 
-app.get('/',(req,res)=>{
-    res.render('home')
+app.get('/',async(req,res)=>{
+    let {data,error} = await supabase.from('students').select('*');
+    res.render('home',{all:data})
 })
 
 app.listen(80,()=>{
-    console.log('server started with port 3000')
+    console.log('server started with port 80')
 })
