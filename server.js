0d6c35caa32ff708c11a17355e961cb998648f3e
Aon Mao <aonmao888@gmail.com>
2023-07-19 21:19:02 +0630

    1

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const ejs = require('ejs');
+const app = express();
+
+app.set('views','./views')
+app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+app.listen(3000,()=>{
+    console.log('server started with port 3000')
+})
