85875590046f864cd15b4355340204458c3c70a9
Aon Mao <aonmao888@gmail.com>
2023-07-20 16:03:34 +0630

    8

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 require('dotenv').config();
 const {createClient} = require('@supabase/supabase-js');
 
-const supabase = createClient(process.env.supabaseurl,process.env.supabasekey)
+const supabase = createClient('https://vfdlyunuvhflycrwlmix.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmZGx5dW51dmhmbHljcndsbWl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk3ODA0NjksImV4cCI6MjAwNTM1NjQ2OX0.6H-gPgS51S6apQuIT39P0xS8mJPrV-It3FYYXspzlkU')
 
 app.set('views',__dirname+'/views')
 app.set('view engine','ejs')
